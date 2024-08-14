29a63774ab53a1def8ac5ee4fd29df208ac87426
Kirtika Jain <jain.kirtika08@gmail.com>
2024-08-15 02:17:50 +0530

    feat: intergrated add trip component to backend.

--- src/components/addtrip.js
+++ src/components/addtrip.js
@@ -6,8 +6,38 @@ import CardActions from "@mui/joy/CardActions";
 import Input from "@mui/joy/Input";
 import Add from "@mui/icons-material/Add";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import axios from "axios";
+import { enqueueSnackbar } from "notistack";
+
+
 
 export default function AddTripCard() {
+  const [destination, setDestination] = useState("");
+  const [startDate, setStartDate] = useState("2024-01-01");
+  const [endDate, setEndDate] = useState("2024-01-01");
+  const { token } = useAuth();
+ 
+
+  const handleAddTrip = async() => {
+    const newTrip = {
+      destination: destination,
+      StartsAt: startDate,
+      EndsAt: endDate,
+    };
+    const headers = {
+      headers: { Authorization: token },
+    };
+    try {
+        const Resonse = await axios.post("/trip/add_trip",newTrip,headers);
+        enqueueSnackbar("User's trip added sucessullly.", { variant: "success" });
+    } catch (error) {
+      console.log("error:", error);
+      enqueueSnackbar(error.message, { variant: "error" });
+    }
+  };
+
   return (
     <Card variant="soft">
       <div>
@@ -19,28 +49,38 @@ export default function AddTripCard() {
             gap: 6,
           }}
         >
-            <Input placeholder="Enter Destination" />
-            <TextField
-              id="date"
-              label="Start Date"
-              type="date"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-            <TextField
-              id="date"
-              label="End Date"
-              type="date"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
+          <Input
+            placeholder="Enter Destination"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+          />
+          <TextField
+            id="date"
+            label="Start Date"
+            value={startDate}
+            type="date"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+          <TextField
+            id="date"
+            label="End Date"
+            value={endDate}
+            type="date"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
           <CardActions>
             <Button
+              type="submit"
               startDecorator={<Add />}
               variant="solid"
               color="primary"
+              onClick={handleAddTrip}
             >
               Add Trip
             </Button>
