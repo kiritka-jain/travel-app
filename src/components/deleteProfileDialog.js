b6bde7216940aec4bbd62fde90f3f697d45a7338
Kirtika Jain <jain.kirtika08@gmail.com>
2024-08-25 23:53:28 +0530

    feat: delete user profile - frontend.

--- src/components/deleteProfileDialog.js
+++ src/components/deleteProfileDialog.js
@@ -5,17 +5,37 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import { useAuth } from "../context/AuthContext";
+import axios from "axios";
+import { enqueueSnackbar } from "notistack";
+import { useNavigate } from "react-router-dom";
+
 
 export default function DeleteProfileDialog() {
   const [open, setOpen] = React.useState(true);
   const { token } = useAuth();
+  const navigate = useNavigate();
 
   const handleClose = () => {
     setOpen(false);
   };
-  const handleDelete = ()=>{
-    
-  }
+
+  const handleDelete = async(e)=>{
+    e.preventDefault();
+    const headers = {
+      headers: { Authorization: token },
+    };
+    try {
+      
+      const response = await axios.delete(`/user/delete_user`,headers);
+      enqueueSnackbar("User profile deleted sucessfully.", {
+        variant: "success",
+      });
+      navigate("/login");
+    } catch (error) {
+      console.log("error:", error);
+      enqueueSnackbar(error.message, { variant: "error" });
+    }
+  };
 
   return (
     <React.Fragment>
@@ -32,7 +52,7 @@ export default function DeleteProfileDialog() {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>No</Button>
-          <Button onClick={handleDelete} autoFocus>
+          <Button type="submit" onClick={handleDelete} autoFocus>
             Yes
           </Button>
         </DialogActions>
